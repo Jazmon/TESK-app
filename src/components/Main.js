0fe64fa3f7485da26962170939761c5c1665b128
Atte Huhtakangas <atte.huhtakangas@gmail.com>
2016-02-16 20:25:48 +0200

    add a test object from react-materialize

--- src/components/Main.js
+++ src/components/Main.js
@@ -5,11 +5,14 @@ import React from 'react';
 
 let yeomanImage = require('../images/yeoman.png');
 
+import { Button } from 'react-materialize';
+
 class AppComponent extends React.Component {
   render() {
     return (
       <div className='index'>
         <img src={yeomanImage} alt='Yeoman Generator' />
+        <Button>Hello World!</Button>
         <div className='notice'>Please edit <code>src/components/Main.js</code> to get started!</div>
       </div>
     );
