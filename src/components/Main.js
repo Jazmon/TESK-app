a570501374292d800d0f1dde7c4697da2494f309
Atte Huhtakangas <atte.huhtakangas@gmail.com>
2016-02-16 20:41:29 +0200

    add router

--- src/components/Main.js
+++ src/components/Main.js
@@ -2,19 +2,24 @@ import 'materialize-css/sass/materialize.scss';
 require('styles/App.scss');
 
 import React from 'react';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 
-let yeomanImage = require('../images/yeoman.png');
+import Layout from './pages/Layout';
+import Test from './pages/Test';
+import Login from './pages/Login';
+
 
 import { Button } from 'react-materialize';
 
 class AppComponent extends React.Component {
   render() {
     return (
-      <div className='index'>
-        <img src={yeomanImage} alt='Yeoman Generator' />
-        <Button>Hello World!</Button>
-        <div className='notice'>Please edit <code>src/components/Main.js</code> to get started!</div>
-      </div>
+      <Router history={hashHistory}>
+        <Route path='/' component={Layout}>
+          <IndexRoute component={Test}></IndexRoute>
+          <Route path='login' name='login' component={Login}></Route>
+        </Route>
+      </Router>
     );
   }
 }
