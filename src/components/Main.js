ae98b76f5a7ebc48bb8fdde1a747b7951111dc67
Atte Huhtakangas <atte.huhtakangas@gmail.com>
2016-02-16 20:25:28 +0200

    change materialize import location

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,4 +1,4 @@
-require('normalize.css');
+import 'materialize-css/sass/materialize.scss';
 require('styles/App.scss');
 
 import React from 'react';
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import 'core-js/fn/object/assign';
-import 'materialize-css/sass/materialize.scss';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/Main';
